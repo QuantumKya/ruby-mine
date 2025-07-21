f4b6f7e29c63e4313ab0d03de4144b1075af6a59
Rohan V-F <106355292+QuantumKya@users.noreply.github.com>
2025-07-21 14:39:48 -0500

    login/signup pages1!!

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,4 +4,7 @@ class PagesController < ApplicationController
   
   def login
   end
+
+  def signup
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get "/game", to: 'pages#game'
   get "/login", to: 'pages#login'
+  get '/signup', to: 'pages#sign_up'
+  root to: 'pages#login'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
